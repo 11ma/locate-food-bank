339fed46e2dbbc2d6e1db162dab1310833361d8a
Michael A <mikeade08l@GMAIL.COM>
2022-08-27 01:28:13 +0100

    created a file for filter data

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("food bank", () => {
+	render(<App />);
+	const linkElement = screen.getByText(/food bank/i);
+	expect(linkElement).toBeInTheDocument();
 });
--- src/components/LocateFood.js
+++ src/components/LocateFood.js
@@ -1,13 +1,13 @@
 import { FoodBankData } from "../api/foodBankAPI";
-import { CalculateDistance } from "./CalculateDistance";
 import { GetLocation } from "../api/getLocation";
 import { BallTriangle } from "react-loader-spinner";
 import { useEffect, useState } from "react";
 import DisplayMap from "./DisplayMap";
 import ListFoodBank from "./ListFoodBank";
+import { FilterData } from "./FilterData";
 
 const LocateFood = () => {
-	const location = GetLocation();
+	const myLocation = GetLocation();
 	const [foodBankData, setFoodBankData] = useState([]);
 	const [err, setErr] = useState(null);
 	const zoom = 13;
@@ -22,33 +22,27 @@ const LocateFood = () => {
 	console.log("error:", err);
 
 	// filter for only 10 miles
-	const filterData = foodBankData.filter((v) => {
-		const getDistance = CalculateDistance(
-			location.lat,
-			location.lng,
-			v.coord.lat,
-			v.coord.lng
-		);
-		if (getDistance < 10) {
-			return true;
-		} else {
-			return false;
-		}
-	});
+	const filterData = FilterData(foodBankData, myLocation);
 
-	// console.log(filterData)
+	if (filterData.length === 0) {
+		console.log("nothing here yet");
+	} else {
+		console.log(filterData);
+	}
 
-	if (foodBankData.length === 0 || location.length === 0) {
+	if (filterData.length === 0 || myLocation.length === 0) {
 		return (
 			<div>
 				<h1>Food banks</h1>
 				<BallTriangle />
 			</div>
 		);
+	} else if (err !== null) {
+		window.location.reload();
 	} else {
 		return (
 			<div>
-				<DisplayMap center={location} data={filterData} zoom={zoom} />
+				<DisplayMap center={myLocation} data={filterData} zoom={zoom} />
 				<ListFoodBank data={filterData} />
 			</div>
 		);
